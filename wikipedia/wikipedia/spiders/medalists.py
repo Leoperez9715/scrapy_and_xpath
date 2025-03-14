01ad72c9ebc56e9cc94d80d50c198f7becf31547
EDWAR PEREZ <leoperez9715@gmail.com>
2025-03-14 14:11:54 -0500

    proyecto_medallistas

--- wikipedia/wikipedia/spiders/medalists.py
+++ wikipedia/wikipedia/spiders/medalists.py
@@ -4,7 +4,7 @@ import scrapy
 class MedalistsSpider(scrapy.Spider):
     name = "medalists"
     allowed_domains = ["wikipedia.org"]
-    start_urls = ["https://wikipedia.org"]
+    start_urls = [r"https://es.wikipedia.org/wiki/Anexo:Pel%C3%ADculas_con_m%C3%A1s_premios_%C3%93scar"]
 
     def parse(self, response):
         pass
